90c6f165dc807c3dd9b6bf11734229baec60fb33
Tonje Røsnes <tonjerrr@gmail.com>
2022-04-29 16:58:39 +0200

    scene view to character view

--- Assets/Scripts/YarnInteractable.cs
+++ Assets/Scripts/YarnInteractable.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
+using UnityEngine.UI;
 
 public class YarnInteractable : MonoBehaviour
 {
     private DialogueRunner dialogueRunner;
     public string conversationStartNode;
 
+    public soPerson person1;
+
+    GameObject sceneSprites;
+    public bool conversationActive;
+
+    public Image image;
+
     public void Start()
     {
+        sceneSprites = GameObject.FindGameObjectWithTag("SceneSprites");
         dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
     }
 
@@ -17,11 +26,28 @@ public class YarnInteractable : MonoBehaviour
     private void StartConversation()
     {
         dialogueRunner.StartDialogue(conversationStartNode);
+        ShowPerson(person1);
+    }
+    public void ShowPerson(soPerson person)
+    {
+        image.sprite = person.Image;
     }
 
     public void OnMouseDown()
     {
-            StartConversation();
+        StartConversation();
+        conversationActive = true;
     }
+
+    private void Update()
+    {
+        if (conversationActive)
+        {
+ 
+            sceneSprites.SetActive(false);
+        }
+    }
+
+
 
 }
